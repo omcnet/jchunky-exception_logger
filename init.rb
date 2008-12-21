bdc1418ba04419794437dbbdaa39aec56a5fcda3
unknown <ol.keene@gmail.com>
2008-12-21 17:14:53 +0200

    adding support for will_paginate as gem

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
+require 'will_paginate' unless Kernel.const_defined? 'WillPaginate'
 WillPaginate.enable
 LoggedExceptionsController.view_paths = [File.join(directory, 'views')]
 LoggedExceptionsMailer.view_paths = File.join(File.dirname(__FILE__), 'views')
