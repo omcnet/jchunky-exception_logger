cf9c9afc26cd5514ea1a9a0f632241ce9e427ae2
Roland Guem <roland.guem@gmail.com>
2010-08-15 13:01:21 +0200

    adapt test files

---
+++ test/test_exception_logger.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestExceptionLogger < Test::Unit::TestCase
+  should "probably should implement tests and start testing for real" do
+    flunk "hey buddy, you should probably test this code!"
+  end
+end
