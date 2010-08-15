6702c64d85afd6a857d726fbb93d58bc660cde2c
Roland Guem <roland.guem@gmail.com>
2010-08-15 12:49:50 +0200

    add new engine file

---
+++ lib/exception_logger/engine.rb
@@ -0,0 +1,13 @@
+require "exception_logger"
+require "rails"
+require "will_paginate"
+require "i18n"
+
+module ExceptionLogger
+  class Engine < Rails::Engine
+    ## Better: TODO create rake task to copy files to application/public
+    initializer "static assets" do |app|
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+    end
+  end
+end
