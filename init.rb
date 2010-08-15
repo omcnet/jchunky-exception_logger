8014d2db43222c6f4e0ff8c7e9c609bc8cb9bc62
Roland Guem <roland.guem@gmail.com>
2010-08-15 12:16:51 +0200

    remove old files (no longer needed for rails3)

--- init.rb
+++
@@ -1,6 +0,0 @@
-require 'will_paginate' unless Kernel.const_defined? 'WillPaginate'
-WillPaginate.enable
-d = File.dirname(__FILE__)
-LoggedExceptionsController.view_paths = [File.join(d, 'views')]
-LoggedExceptionsMailer.template_root= File.join(d, 'views')
-ExceptionLoggableControllerMixin.custom_view_paths = [File.join(d, 'views')]
